58968d2dd95fe3b64a0ce47eafb2f61f693f23f1
SzymBurz <szb210@gmail.com>
2022-05-08 21:43:46 +0200

    ForumUser

---
+++ kodilla-spring/src/test/java/com/kodilla/kodillaspring/forum/ForumUserTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.kodillaspring.forum;
+
+import com.kodilla.spring.forum.ForumUser;
+import com.kodilla.spring.shape.Circle;
+import com.kodilla.spring.shape.Shape;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ForumUserTestSuite {
+
+    @Test
+    void testGetUsername() {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+
+
+        String username = forumUser.getUsername();
+
+        assertEquals("John Smith", username);
+
+    }
+}
